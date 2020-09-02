1312a10943f7d3c64999c22013c53da4ae538fb6
Watanabe0624 <meijimarble@icloud.com>
2020-09-02 21:36:46 +0900

    [fix]命名の変更

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BattleRecordPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
